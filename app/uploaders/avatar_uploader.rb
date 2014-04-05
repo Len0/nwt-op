e9bef958a95d79cfbae5ba1cf90d189e475f4c94
Irfan Prazina <irfanpra@gmail.com>
2014-04-05 18:55:42 +0200

    Stavljena defaultna slika za profil

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,6 +15,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  def default_url
+    'bluebells_darker.jpg'
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
