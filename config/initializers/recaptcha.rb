9e2d71fc210239d8165bd8f007a87c47b9512dcf
Irfan Prazina <irfanpra@gmail.com>
2014-04-22 21:52:06 +0200

    recaptcha i na heroku

--- config/initializers/recaptcha.rb
+++
@@ -1,3 +0,0 @@
-# RADI na localost, treba promijeniti ako se bude hostalo
-ENV['RECAPTCHA_PUBLIC_KEY'] = '6Lfo4fASAAAAAEa7UXW2bp_AwWzi6EcVA8D5W3fi '
-ENV['RECAPTCHA_PRIVATE_KEY'] = '  6Lfo4fASAAAAAHWsG2y-xl7LBtyKnzJUxedDz1xF'
