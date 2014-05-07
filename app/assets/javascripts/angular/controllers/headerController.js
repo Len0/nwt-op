7c43e35885f78d6c0a4315c2b1c929b9beb8cdb0
Len0 <alen.hadzic91@gmail.com>
2014-05-07 21:53:40 +0200

    login

--- app/assets/javascripts/angular/controllers/headerController.js
+++ app/assets/javascripts/angular/controllers/headerController.js
@@ -5,9 +5,23 @@ App.controller ('headerController', ['$scope','$http', function($scope,$http){
         	});
         };
         $scope.userLogout = function(){
-            $http.get('user/logout').success(function(){
+            $http.get('/user/logout').success(function(){
                 location.reload(true);
             })
 
         };
+            $scope.userLogin = function(user){
+                $http({
+                    url: '/user/login',
+                    method: "POST",
+                    data:{username: user.username, password: user.password},
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).success(function (data, status, headers, config) {
+
+                        $scope.data = data;
+                    }).error(function (data, status, headers, config) {
+                        $scope.status = status;
+                    });
+
+            };
 }]);
